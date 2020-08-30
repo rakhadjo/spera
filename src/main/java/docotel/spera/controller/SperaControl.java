7d67b862c4e3d3010687d2fc28dd8524479917eb
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-30 23:48:06 +0700

    x

--- src/main/java/docotel/spera/controller/SperaControl.java
+++ src/main/java/docotel/spera/controller/SperaControl.java
@@ -5,14 +5,15 @@
  */
 package docotel.spera.controller;
 
-import docotel.spera.models.Grade;
-import docotel.spera.models.Position;
 import docotel.spera.repositories.GradeRepository;
 import docotel.spera.repositories.PositionRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,16 +32,16 @@ public class SperaControl {
     private GradeRepository gradeRepo;
     
     @GetMapping("/troops/positions")
-    public ResponseEntity<List<Position>> allPositions() {
-        return ResponseEntity.accepted()
-                .header("x-trace-id", "x-trace-id")
-                .body(positionRepo.findAll());
+    public ResponseEntity<List> allPositions(@RequestHeader("Authentication") String Authentication) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("x-trace-id", "x-trace-id");
+        return new ResponseEntity<>(positionRepo.findAll(), headers, HttpStatus.OK);
     }
     
     @GetMapping("/troops/grades")
-    public ResponseEntity<List<Grade>> allGrades() {
-        return ResponseEntity.accepted()
-                .header("x-trace-id", "x-trace-id")
-                .body(gradeRepo.findAll());
+    public ResponseEntity<List> allGrades() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("x-trace-id", "x-trace-id");
+        return new ResponseEntity<>(gradeRepo.findAll(), headers, HttpStatus.OK);
     }
 }
