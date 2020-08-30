0454b38c77c971cabf42f8da3335fb2d1fab3955
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-30 17:26:46 +0700

    init commit

---
+++ src/main/java/docotel/spera/SperaApplication.java
@@ -0,0 +1,13 @@
+package docotel.spera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SperaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/docotel/spera/SperaApplicationTests.java
@@ -0,0 +1,13 @@
+package docotel.spera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
