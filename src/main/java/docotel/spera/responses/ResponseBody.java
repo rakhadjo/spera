3b9dc60d7e4f63af5550814f188d02f504da476e
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-30 22:25:14 +0700

    add: ResponseBody.java for inheritance later

---
+++ src/main/java/docotel/spera/responses/ResponseBody.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package docotel.spera.responses;
+
+/**
+ *
+ * @author rakhadjo
+ */
+public class ResponseBody {
+
+    private String rc;
+    private String message;
+    private final String role = "admin";
+    private final String menu = "home, troops, account";
+
+    public ResponseBody(Result res) {
+        setCodes(res);
+    }
+
+    public void setCodes(Result res) {
+        switch (res) {
+            case SUCCESS:
+                this.rc = "00";
+                this.message = "success";
+                break;
+            case ERROR:
+                this.rc = "99";
+                this.message = "internal error";
+                break;
+        }
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public org.bson.Document toJSON() {
+        return new org.bson.Document()
+                .append("rc", rc)
+                .append("message", message)
+                .append("role", role)
+                .append("menu", menu);
+                
+    }
+
+}
