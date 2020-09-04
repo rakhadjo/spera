dfc561fc96e6c344db89ed6f3ddeefe854dd7e52
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-09-04 13:53:34 +0700

    add: response element for /troops/list

---
+++ src/main/java/docotel/spera/responses/GetTroopsResponse.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package docotel.spera.responses;
+
+import docotel.spera.models.Troop;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rakhadjo
+ */
+public class GetTroopsResponse extends ResponseBody {
+    
+    public java.util.List<Troop> data;
+    
+    public GetTroopsResponse(Result res) {
+        super(res);
+    }
+    
+    public org.bson.Document toJSON() {
+        ArrayList data2 = new ArrayList();
+        for (Troop t : data) {
+            data2.add(t.toJSON());
+        }
+        return super.toJSON()
+                .append("data", data2)
+                ;
+    }
+    
+}
