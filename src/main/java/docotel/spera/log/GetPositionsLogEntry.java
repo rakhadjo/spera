a0fe51038383e03897fe5159992e418466686e0c
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-09-04 18:10:45 +0700

    add: LogEntry object for method /troops/positions

---
+++ src/main/java/docotel/spera/log/GetPositionsLogEntry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package docotel.spera.log;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author rakhadjo
+ */
+@Document(collection = "log_getpositions")
+public class GetPositionsLogEntry extends LogEntry {
+    
+    public GetPositionsLogEntry() {}
+    
+}
